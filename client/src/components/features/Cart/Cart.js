e2ca7a2475b641276139a186291c23a95c7ef761
Marysia Pabich <marysia.pabich@gmail.com>
2020-03-31 19:03:45 +0200

    Create basics for Cart component, incl. Cart expand

---
+++ client/src/components/features/Cart/Cart.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import clsx from 'clsx';
+
+// import { connect } from 'react-redux';
+// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+
+import styles from './Cart.module.scss';
+
+const Component = ({ className, children }) => {
+
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className={clsx(className, styles.root)}>
+      <CardActions className={styles.head} disableSpacing>
+        <IconButton
+          className={`${styles.expand} ${expanded ? styles.expandOpen : ''}`}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography>
+            Podsumowanie zamówienia
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+};
+
+Component.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
+
+// const mapStateToProps = state => ({
+//   someProp: reduxSelector(state),
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//   someAction: arg => dispatch(reduxActionCreator(arg)),
+// });
+
+// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+
+export {
+  Component as Cart,
+  // Container as Cart,
+  Component as CartComponent,
+};
