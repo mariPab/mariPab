e452250dfc857a7064b824ff3793a93ccc00bace
Marysia Pabich <marysia.pabich@gmail.com>
2020-03-31 23:50:20 +0200

    Create model of order

---
+++ models/order.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const orderedProductsSchema = new mongoose.Schema({
+  id: { type: ObjectId, required: true, ref: 'Product' },
+  amount: { type: Number, required: true },
+});
+
+const clientSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  address: { type: String, required: true },
+  postCode: { type: String, required: true },
+});
+
+const orderSchema = new mongoose.Schema({
+  orderedProducts: [orderedProductsSchema],
+  client: [clientSchema],
+  orderValue: { type: Number, required: true },
+},
+  { versionKey: false }
+);
+
+module.exports = mongoose.model('Order', orderSchema);
