47597c7ba2ac07a6ea05448c60756666f0e560c9
Marysia Pabich <marysia.pabich@gmail.com>
2020-03-31 17:26:41 +0200

    Confugure basics of server

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+// const mongoose = require('mongoose');
+
+// const customRoutes = require('./routes/custom.routes');
+
+const app = express();
+
+/* MIDDLEWARE */
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+/* API ENDPOINTS */
+// app.use('/api', customRoutes);
+
+/* API ERROR PAGES */
+app.use('/api', (req, res) => {
+  res.status(404).send({ data: 'Not found...' });
+});
+
+/* REACT WEBSITE */
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+/* START SERVER */
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log('Server is running on port: ' + port);
+});
