aa429fdd564bf1e8b106fae62d33c51d39b1d79a
Marysia Pabich <marysia.pabich@gmail.com>
2020-04-01 21:21:49 +0200

    Move photos to server side

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,2 @@
 export const API_URL = (process.env.NODE_ENV === 'production') ? '/api' : 'http://localhost:8000/api';
+export const IMAGES_URL = (process.env.NODE_ENV === 'production') ? '/images' : 'http://localhost:8000/images';
--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, './public')));
+
 /* API ENDPOINTS */
 app.use('/api', require('./routes/products.routes'));
 
