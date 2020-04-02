2384d8fa27e1eb450e41f0c5694435424cdf23ee
Marysia Pabich <marysia.pabich@gmail.com>
2020-04-02 22:18:46 +0200

    Set Cart in header

--- client/src/components/layout/Header/Header.js
+++ client/src/components/layout/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { Cart } from '../../features/Cart/Cart';
 import clsx from 'clsx';
 
 // import { connect } from 'react-redux';
@@ -8,9 +8,10 @@ import clsx from 'clsx';
 
 import styles from './Header.module.scss';
 
-const Component = ({className, children}) => (
+const Component = ({ className, children }) => (
   <div className={clsx(className, styles.root)}>
-    <h2>Header</h2>
+    <h1>Natural Beauty Shop</h1>
+    <Cart />
     {children}
   </div>
 );
--- client/src/components/layout/MainLayout/MainLayout.js
+++ client/src/components/layout/MainLayout/MainLayout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { Header } from '../Header/Header';
 import clsx from 'clsx';
 
 // import { connect } from 'react-redux';
@@ -8,9 +8,9 @@ import clsx from 'clsx';
 
 import styles from './MainLayout.module.scss';
 
-const Component = ({className, children}) => (
+const Component = ({ className, children }) => (
   <div className={clsx(className, styles.root)}>
-    <h2>MainLayout</h2>
+    <Header />
     {children}
   </div>
 );
