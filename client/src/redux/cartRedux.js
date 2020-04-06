f5446bdf308c9b224e59bf6700c1bf35dcd82588
Marysia Pabich <marysia.pabich@gmail.com>
2020-04-06 19:17:53 +0200

    Create action to send order

--- client/src/redux/cartRedux.js
+++ client/src/redux/cartRedux.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { API_URL } from '../config';
+
 /* selectors */
 export const getCart = ({ cart }) => cart;
 export const getProductFromCart = ({ cart }, productId) => cart.products.filter(product => product._id === productId)[0];
@@ -14,6 +17,7 @@ const FETCH_ERROR = createActionName('FETCH_ERROR');
 const ADD_TO_CART = createActionName('ADD_TO_CART');
 const CHANGE_AMOUNT = createActionName('CHANGE_AMOUNT');
 const REMOVE_FROM_CART = createActionName('REMOVE_FROM_CART');
+const SEND_ORDER = createActionName('SEND_ORDER');
 
 /* action creators */
 export const fetchStarted = payload => ({ payload, type: FETCH_START });
@@ -22,8 +26,28 @@ export const fetchError = payload => ({ payload, type: FETCH_ERROR });
 export const addProductToCart = payload => ({ payload, type: ADD_TO_CART });
 export const changeProductAmount = payload => ({ payload, type: CHANGE_AMOUNT });
 export const removeFromCart = payload => ({ payload, type: REMOVE_FROM_CART });
+export const sendOrder = payload => ({ payload, type: SEND_ORDER });
 
 /* thunk creators */
+export const newOrderRequest = data => {
+  return async dispatch => {
+    dispatch(fetchStarted());
+    try {
+      let res = await axios.post(
+        `${API_URL}/order`,
+        data,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      dispatch(sendOrder(res.data));
+    } catch (e) {
+      dispatch(fetchError(e.message));
+    }
+  };
+};
 
 /* reducer */
 export const reducer = (statePart = [], action = {}) => {
@@ -91,6 +115,15 @@ export const reducer = (statePart = [], action = {}) => {
         ...statePart,
         products: statePart.products.filter(product => product._id !== action.payload._id),
         total: statePart.total,
+      };
+    }
+    case SEND_ORDER: {
+      return {
+        ...statePart,
+        order: {
+          products: [],
+          total: 0,
+        },
       };
     }
     default:
