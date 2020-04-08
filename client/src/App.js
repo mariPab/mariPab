8b19c6400cd3bb990c2bc84081df7118881cbea9
Marysia Pabich <marysia.pabich@gmail.com>
2020-04-08 22:51:39 +0200

    Animate switch between view

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { AnimatedSwitch } from 'react-router-transition';
 import { Provider } from 'react-redux';
 
 import { createMuiTheme, StylesProvider, ThemeProvider } from '@material-ui/core/styles';
@@ -12,6 +13,8 @@ import { Homepage } from './components/views/Homepage/Homepage';
 import { Product } from './components/views/Product/Product';
 import { OrderSummary } from './components/views/OrderSummary/OrderSummary';
 import { NotFound } from './components/views/NotFound/NotFound';
+
+import styles from './App.module.scss';
 
 const theme = createMuiTheme({
   palette: {
@@ -27,12 +30,17 @@ const App = () => (
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <MainLayout>
-            <Switch>
+            <AnimatedSwitch
+              atEnter={{ opacity: 0 }}
+              atLeave={{ opacity: 0 }}
+              atActive={{ opacity: 1 }}
+              className={styles.switchWrapper}
+            >
               <Route exact path='/' component={Homepage} />
               <Route exact path='/product/:id' component={Product} />
               <Route exact path='/order' component={OrderSummary} />
               <Route path='*' component={NotFound} />
-            </Switch>
+            </AnimatedSwitch>
           </MainLayout>
         </ThemeProvider>
       </StylesProvider>
