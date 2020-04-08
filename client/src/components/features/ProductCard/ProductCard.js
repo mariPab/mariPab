317e311c9e5c5fa9701c897c1da6ab4f2c25304b
Marysia Pabich <marysia.pabich@gmail.com>
2020-04-08 20:38:59 +0200

    Remove unused import

--- client/src/components/features/ProductCard/ProductCard.js
+++ client/src/components/features/ProductCard/ProductCard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
-import Typography from '@material-ui/core/Typography';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Fab from '@material-ui/core/Fab';
