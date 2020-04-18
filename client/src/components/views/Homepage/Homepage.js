d5ec2e4b874ac97c28123bdffcd03af4632e3e77
Marysia Pabich <marysia.pabich@gmail.com>
2020-04-18 18:00:22 +0200

    Add motto

--- client/src/components/views/Homepage/Homepage.js
+++ client/src/components/views/Homepage/Homepage.js
@@ -4,12 +4,15 @@ import { connect } from 'react-redux';
 import { getAll, loadProductsRequest } from '../../../redux/productsRedux.js';
 import { ProductCard } from '../../features/ProductCard/ProductCard';
 import Grid from '@material-ui/core/Grid';
+import styles from './Homepage.module.scss';
+import { getViewportMode } from '../../../redux/viewportRedux.js';
 
 class Component extends React.Component {
   static propTypes = {
     className: PropTypes.string,
     products: PropTypes.array,
     loadProducts: PropTypes.func,
+    mobile: PropTypes.bool,
   };
 
   componentDidMount() {
@@ -17,19 +20,29 @@ class Component extends React.Component {
   }
 
   render() {
-    const { products } = this.props;
+    const { products, mobile } = this.props;
     return (
-      <Grid container>
-        {products.map(product => (
-          <ProductCard key={product._id} {...product} />
-        ))}
-      </Grid>
+      <div className={styles.root}>
+        <section className={`${styles.motto} ${mobile ? styles.mobileOnly : ''}`}>
+          <p>
+            Kosmetyki Natural Beauty zostały stworzone z myślą o naturze.
+            Składniki dobrane z najwyższą starannością dbają nie tylko o Twoją skórę, ale również o nasze otoczenie.
+          </p>
+        </section>
+        <Grid container>
+          {products.map(product => (
+            <ProductCard key={product._id} {...product} />
+          ))}
+        </Grid>
+      </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
   products: getAll(state),
+  mobile: getViewportMode(state),
+
 });
 
 const mapDispatchToProps = dispatch => ({
