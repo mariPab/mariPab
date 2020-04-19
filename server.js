e3bd86f8777a5411986764f94ffbe75f18b23b3d
Marysia Pabich <marysia.pabich@gmail.com>
2020-04-19 20:56:01 +0200

    Remove if statement

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname + '/public')));
-app.use(express.static(path.join(__dirname + '/client')));
-
-if (process.env.NODE_ENV === 'production') app.use(express.static(path.join(__dirname + '/client/build')));
+app.use(express.static(path.join(__dirname + '/client/build')));
 
 /* START SERVER */
 const port = process.env.PORT || 8000;
