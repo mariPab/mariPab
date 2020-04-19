825719e844dad85fdb615c779ebf353b7254f595
Marysia Pabich <marysia.pabich@gmail.com>
2020-04-19 11:55:24 +0200

    Ad link to main page, style Not Found page

--- client/src/components/views/NotFound/NotFound.js
+++ client/src/components/views/NotFound/NotFound.js
@@ -1,34 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
-import clsx from 'clsx';
-
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
-
 import styles from './NotFound.module.scss';
+import { NavLink } from 'react-router-dom';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>NotFound</h2>
-    {children}
+const Component = () => (
+  <div className={styles.root}>
+    <p>Strona nie została znaleziona</p>
+    <NavLink exact to='/'>
+      Powrót do strony głównej
+    </NavLink>
   </div>
 );
-
-Component.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-};
-
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
-
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
-
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
   Component as NotFound,
