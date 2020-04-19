053d373deb73399cb0da5d8bc399f3de4fcaedfc
Marysia Pabich <marysia.pabich@gmail.com>
2020-04-19 11:47:27 +0200

    Clear code

--- client/src/components/features/ProductCard/ProductCard.js
+++ client/src/components/features/ProductCard/ProductCard.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
-import clsx from 'clsx';
 import { IMAGES_URL } from '../../../config';
 import Grid from '@material-ui/core/Grid';
 import styles from './ProductCard.module.scss';
 import { Button } from '../../common/Button/Button';
 
-const Component = ({ className, _id, name, images, price }) => (
-  <Grid item xs={10} sm={5} lg={3} className={clsx(className, styles.root)}>
+const Component = ({ _id, name, images }) => (
+  <Grid item xs={10} sm={5} lg={3} className={styles.root}>
     <div className={styles.root}>
       <img src={`${IMAGES_URL}/${images[0]}`} alt={name} />
       <div className={styles.productDetails}>
@@ -33,18 +32,7 @@ Component.propTypes = {
   className: PropTypes.string,
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
-
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
-
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
-
 export {
   Component as ProductCard,
-  // Container as Product,
   Component as ProductCardComponent,
 };
