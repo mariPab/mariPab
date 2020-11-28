8eaeb58c1db5eec1e806d01d6725a09fc1e9ecf5
Marysia Pabich <marysia.pabich@gmail.com>
2020-11-28 19:27:49 +0100

    Change scrollbar for body

--- client/src/styles/global.tsx
+++ client/src/styles/global.tsx
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import { variables } from './settings';
+import { lighten } from 'polished';
 
 export const GlobalStyle = createGlobalStyle`
   *,
@@ -15,5 +16,14 @@ export const GlobalStyle = createGlobalStyle`
   body {
     font: 100 1.4rem ${variables.fontText};
     margin: 0;
+    &::-webkit-scrollbar {
+      -webkit-appearance: none;
+      width: 4px;
+      height: 4px;
+      background: ${variables.colorSupplemental};
+    }
+    &::-webkit-scrollbar-thumb {
+      background: ${lighten(0.1, variables.colorBorder)};
+    }
   }
 `;
