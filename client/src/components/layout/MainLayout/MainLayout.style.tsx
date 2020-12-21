6ec93040da83873e7fb6b0cb4ab3a84557a3b5e9
Marysia Pabich <marysia.pabich@gmail.com>
2020-12-21 19:46:44 +0100

    set constant height for footer and calc height fot page container

--- client/src/components/layout/MainLayout/MainLayout.style.tsx
+++ client/src/components/layout/MainLayout/MainLayout.style.tsx
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 
 export const PageContainer = styled.div`
   min-height: 100vh;
+  overflow-y: auto;
 `;
 
 export const ContentContainer = styled.div`
   margin: 7rem 5rem 0 5rem;
+  height: calc(100vh - 270px);
 `;
