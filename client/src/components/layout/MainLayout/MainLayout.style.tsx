e577fdd2f90e03886e9f4a2b6e72ea2196cf0604
Marysia Pabich <marysia.pabich@gmail.com>
2020-12-25 23:27:34 +0100

    add max-width for page container & auto margin

--- client/src/components/layout/MainLayout/MainLayout.style.tsx
+++ client/src/components/layout/MainLayout/MainLayout.style.tsx
@@ -6,8 +6,9 @@ export const PageContainer = styled.div`
 `;
 
 export const ContentContainer = styled.div`
-  margin: 7rem 5rem 0 5rem;
+  margin: 7rem auto 0 auto;
   height: calc(100vh - 270px);
+  max-width: 900px;
   @media (max-width: 567px){
     margin: 7rem 2rem 0 2rem;
     height: calc(100vh - 210px);
